a352fcd0d066ab171903fc47b6dd017f6c62a06c
Ayom04 <abdullahayomide04@gmail.com>
2023-05-30 11:46:45 +0100

    complying with betty

--- binary_trees.h
+++ binary_trees.h
@@ -9,7 +9,7 @@
 /*Basic binary tree*/
 /**
  * struct binary_tree_s - Binary tree node
- * 
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -17,10 +17,10 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
 
