46f11ec2a6e24a94bd9465318e489b8492d4741e
Ayom04 <abdullahayomide04@gmail.com>
2023-06-01 07:46:17 +0100

    comply eith betty

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,13 +2,13 @@
 
 /**
  * binary_tree_delete - deletes an entire binary tree
- * 
+ *
  * @tree: a pointer to the root node of the tree to delete
  * If tree is NULL, do nothing
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
-    if (tree == NULL)
+	if (tree == NULL)
 		return;
 
 	binary_tree_delete(tree->left);
