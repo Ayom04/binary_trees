a627e002262017a227e71ac054b8b441063deff4
Ayom04 <abdullahayomide04@gmail.com>
2023-06-01 12:28:01 +0100

    binary search is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,18 @@
 #include "binary_trees.h"
 /**
+ * comparing_depth - compare size
+ *
+ * @tree: pointer to the root
+ * Return: size
+ */
+int comparing_depth(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	else
+		return (comparing_depth(tree->left) + 1 + comparing_depth(tree->right));
+}
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  *
  * @tree: pointer to the root node of the tree to check
@@ -15,8 +28,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree && (!tree->left && !tree->right))
 		return (1);
 
-	height_left = compare_depth(tree->left);
-	height_right = compare_depth(tree->right);
+	height_left = comparing_depth(tree->left);
+	height_right = comparing_depth(tree->right);
 
 	if ((height_left - height_right) == 0)
 		return (1);
