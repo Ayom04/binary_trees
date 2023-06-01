09e8728c366e405b26fd654f728c0b0dd211d207
Ayom04 <abdullahayomide04@gmail.com>
2023-06-01 18:24:53 +0100

    avl insert

--- binary_trees.h
+++ binary_trees.h
@@ -85,4 +85,9 @@ bst_t *bst_remove(bst_t *root, int value);
 size_t height(const binary_tree_t *tree);
 int is_avl_helper(const binary_tree_t *tree, int lo, int hi);
 int binary_tree_is_avl(const binary_tree_t *tree);
+size_t height(const binary_tree_t *tree);
+int balance(const binary_tree_t *tree);
+avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
+		avl_t **new, int value);
+avl_t *avl_insert(avl_t **tree, int value);
 #endif
