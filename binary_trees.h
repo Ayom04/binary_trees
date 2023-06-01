55268d23d4f4967b87395e6c593315272233738e
Ayom04 <abdullahayomide04@gmail.com>
2023-06-01 17:47:15 +0100

    binary tree is best

--- binary_trees.h
+++ binary_trees.h
@@ -71,6 +71,8 @@ int binary_tree_is_complete(const binary_tree_t *tree);
 int custom_height(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-int isBST(const binary_tree_t *tree, int min, int max);
-int binary_tree_is_bst(const binary_tree_t *tree)
+int binary_tree_is_bst(const binary_tree_t *tree);
+int is_bst_helper(const binary_tree_t *tree, int *largest);
+int is_left(const binary_tree_t *node);
+int is_right(const binary_tree_t *node);
 #endif
