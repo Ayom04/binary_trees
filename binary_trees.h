33384bd853739f5d02ef69fd3d6abc57338c1079
Ayom04 <abdullahayomide04@gmail.com>
2023-06-01 20:00:06 +0100

    heat to sorted array

--- binary_trees.h
+++ binary_trees.h
@@ -101,4 +101,5 @@ heap_t *heap_nf(heap_t *root, size_t number);
 heap_t *heap_r(heap_t *new, heap_t *parent);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 #endif
